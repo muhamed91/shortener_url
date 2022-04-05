9dce9f34ec1dbf3fcf9d70f67e76f0e2f3873baa
Muhamed Rustemi <muhamedrustemi91@gmail.com>
2022-04-05 17:28:33 +0200

    Add new Service for the Shortener and improve some issues in JS

--- js/main.js
+++ js/main.js
@@ -2,100 +2,88 @@ const menu = document.querySelector('.menu-icon');
 const mobileNav = document.querySelector('.mobile-nav_panel');
 var submitButton = document.querySelector('#myForm');
 var shortenBtn = document.querySelector('.btn-submit');
+var clearBtn = document.querySelector('.btn-danger');
 var inputField = document.querySelector('.input-field');
 var mainContainer = document.getElementById("myData");
 
+menu.addEventListener('click', function () {
+    mobileNav
+        .classList
+        .toggle('show');
 
-
-
-menu.addEventListener('click', function() {
-    mobileNav.classList.toggle('show');
-    
-    
-
-    if(mobileNav.classList.contains('show')) {
+    if (mobileNav.classList.contains('show')) {
         menu.setAttribute('class', 'close-icon')
         menu.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
     } else {
-        menu.classList.remove('show');
+        menu
+            .classList
+            .remove('show');
         menu.removeAttribute('class', 'close-icon')
         menu.innerHTML = '<i class="fa fa-bars menu-icon" aria-hidden="true"></i>'
-    }   
+    }
 
 })
 
-
-
-
-
 function postLink(input) {
-    fetch('https://rel.ink/api/links/', {
+
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("url", input);
+
+    localStorage.setItem('oldUrl', JSON.stringify(input));
+
+    fetch('https://url-shortener-service.p.rapidapi.com/shorten', {
         method: 'POST',
         headers: {
-           
-            'Accept': 'application/json, text/plain, /*/',
-            "Content-type": "application/json",
-             
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com',
+            'X-RapidAPI-Key': '8e0ae555d4msh776103877605b39p1052dcjsn51d99b4d1768'
         },
-        body: JSON.stringify({
-            url: input
+        body: encodedParams
+
+    })
+        .then((res) => res.json())
+        .then(data => {
+            getShortLink(data.result_url);
+
         })
-    })
-    .then((res) => res.json())
-    .then(data =>  {
-        getShortLink(data.hashid)
-    })
-   
+
 }
 
+async function getShortLink(response) {
 
-async function getShortLink(antwort) {
-        
-    await fetch('https://rel.ink/api/links/' + antwort)
-    .then((response => {
-        return response.json();
-    }))
-    .then(data => {
-        console.log(data.url);
-        var listItems = document.createElement('li');
-        var shortUrl = 'https://rel.ink/' + data.hashid
-        var dataUrl = data.url;
-        listItems.innerText = 'https://rel.ink/nWvX3k';
-        // mainContainer.appendChild(listItems);
-        buildElement(dataUrl, shortUrl);
-        storeShortLinkInLs(data);
+    let myObj = {
+        baseUrl: localStorage.getItem('oldUrl'),
+        shortUrl: response
+    }
 
-        function storeShortLinkInLs(data) {
-            let url;
-            if(localStorage.getItem('shortUrl') === null) {
-                url = [];
-            } else {
-                url = JSON.parse(localStorage.getItem('shortUrl'));
-            }
+    document.createElement('li');
+    var shortUrl = response
+    var dataUrl = myObj.baseUrl
+    buildElement(dataUrl, shortUrl);
 
-            url.push(data);
+    storeShortLinkInLs(myObj);
 
-            localStorage.setItem('shortUrl', JSON.stringify(url));
+    function storeShortLinkInLs(data) {
+        let url;
+        if (localStorage.getItem('shortObj') === null) {
+            url = [];
+        } else {
+            url = JSON.parse(localStorage.getItem('shortObj'));
         }
 
+        url.push(data);
 
-        
-    })
-    
+        localStorage.setItem('shortObj', JSON.stringify(url));
+    }
 
-    
-    
 }
 
-
 function buildElement(url, shortUrl) {
-
 
     // Create an LI as Node for UL
     var listItems = document.createElement('li');
     listItems.setAttribute('class', 'list-items');
     mainContainer.appendChild(listItems);
-
 
     // Anchor inside the Ddiv
     var longLinkAnchor = document.createElement('a');
@@ -122,73 +110,91 @@ function buildElement(url, shortUrl) {
     btn.textContent = 'Copy';
     shortLink.appendChild(btn);
 
-
     const textAnchor = document.querySelectorAll('.shorten-link');
     const btnAnchor = document.querySelectorAll('.shorten-link_btn');
 
     // Remove first
 
-   function removeClasses() {
-       btnAnchor.forEach(function(btns) {
-           btns.classList.remove('btn-violett');
-           btns.textContent = 'Copy';
-       })
-   }
+    function removeClasses() {
+        btnAnchor.forEach(function (btns) {
+            btns
+                .classList
+                .remove('btn-violett');
+            btns.textContent = 'Copy';
+        })
+    }
 
     // List all Button
 
-    btnAnchor.forEach(function(btns, index) {
-        btns.addEventListener('click',function(e){
+    btnAnchor.forEach(function (btns, index) {
+        btns.addEventListener('click', function (e) {
             removeClasses();
-            btns.classList.add('btn-violett');
-            copyToClipboard(textAnchor[index].textContent);
+            btns
+                .classList
+                .add('btn-violett');
             btns.textContent = 'Copied!'
+            btnText = textAnchor[index].textContent;
+            btns.setAttribute('data-url', btnText)
+            navigator
+                .clipboard
+                .writeText(btns.dataset.url)
+                .then(function () {
+                    this.dataset
+                })
         });
-        
+
     })
 
-    
 }
 
-// GetLinks from Localstorage
 
-submitButton.addEventListener('submit', function(e){
+
+clearBtn.addEventListener('click', function(e) {
+    localStorage.clear();
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+    
+})
+
+
+submitButton.addEventListener('submit', function (e) {
     e.preventDefault();
-    let input = inputField.value
-    input.trim();
+    let input = inputField
+        .value
+        input
+        .trim();
 
-    if(input === '') {
+    if (input === '') {
         inputField.style.border = '3px solid red';
         shortenBtn.style.backgroundColor = 'red';
 
-        setTimeout(function(){
+        setTimeout(function () {
             shortenBtn.style.backgroundColor = '';
             inputField.style.border = '';
             inputField.value = ''
-        },3000)
-    } else if(!isHttpComp(input)) {
+        }, 3000)
+    } else if (!isHttpComp(input)) {
         inputField.style.border = '3px solid red';
         shortenBtn.style.backgroundColor = 'red';
         inputField.placeholder = 'Write Correct address: http://';
         inputField.value = '';
-        
-        setTimeout(function(){
-            
+
+        setTimeout(function () {
+
             inputField.style.border = '';
             shortenBtn.style.backgroundColor = '';
             inputField.placeholder = 'Shorten a link here...';
             inputField.value = '';
 
-        },3000)
+        }, 3000)
 
     } else {
         postLink(inputField.value);
         inputField.value = ''
-        
 
     }
-    
-    
+
 })
 
 function isHttpComp(input) {
@@ -199,22 +205,17 @@ function isHttpComp(input) {
 function getLinks() {
 
     let url;
-    if(localStorage.getItem('shortUrl') === null) {
+    if (localStorage.getItem('shortObj') === null) {
         url = [];
     } else {
-        url = JSON.parse(localStorage.getItem('shortUrl'));
+        url = JSON.parse(localStorage.getItem('shortObj'));
     }
 
-   url.forEach(function(urls) {
-       let link = 'https://rel.ink/' + urls.hashid;
-       buildElement(urls.url, link)
-      
-   })
+    url.forEach(function (urls) {
 
-   
+        buildElement(urls.baseUrl, urls.shortUrl)
+
+    })
+
 }
 getLinks();
-
-
-
-
